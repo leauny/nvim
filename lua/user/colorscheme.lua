06d43bbcb9c6d9180c115b1efedf341989d30f55
leauny <leauny@outlook.com>
2024-06-03 21:04:45 +0800

    change colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,11 @@
 local M = {
-  "LunarVim/darkplus.nvim",
+  "shaunsingh/nord.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  vim.cmd.colorscheme "nord"
 end
 
 return M
