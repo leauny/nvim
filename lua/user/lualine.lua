9a1da8b59d5558dbfab20bb54b23c5c5484b6ebc
leauny <leauny@outlook.com>
2024-06-08 15:23:15 +0800

    optm: lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -8,17 +8,22 @@ local M = {
 function M.config()
   require("lualine").setup {
     options = {
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
+      theme = 'nord',
+      component_separators = '',
+      section_separators = { left = '', right = '' },
       ignore_focus = { "NvimTree" },
     },
     sections = {
-      lualine_a = {},
+      lualine_a = {
+        { 'mode', separator = { left = '' }, right_padding = 3 }
+      },
       lualine_b = { "branch" },
       lualine_c = { "diagnostics" },
       lualine_x = { "copilot", "filetype" },
       lualine_y = { "progress" },
-      lualine_z = {},
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 3 },
+      },
     },
     extensions = { "quickfix", "man", "fugitive" },
   }
