9637d3041f69c3585335344501fbefe833135a1a
leauny <leauny@outlook.com>
2024-06-08 15:51:56 +0800

    optm: turn on line blame

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -63,6 +63,14 @@ M.config = function()
       follow_files = true,
     },
     attach_to_untracked = true,
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+      delay = 10,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+    },
     current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
     update_debounce = 200,
     max_file_length = 40000,
